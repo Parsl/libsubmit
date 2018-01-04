9c1d0fb7f1999b889862ed7b35df7e4b1767cb54
Yadu Nand B <yadudoc1729@gmail.com>
2018-01-04 14:35:12 -0600

    Adding aprun launcher

--- libsubmit/launchers/__init__.py
+++ libsubmit/launchers/__init__.py
@@ -1,7 +1,9 @@
 from libsubmit.launchers.launchers import singleNodeLauncher
 from libsubmit.launchers.launchers import srunLauncher
 from libsubmit.launchers.launchers import srunMpiLauncher
+from libsubmit.launchers.launchers import aprunLauncher
 
 Launchers = { "singleNode" : singleNodeLauncher,
-              "srun" : srunLauncher,
-              "srun_mpi" : srunMpiLauncher }
+              "srun"       : srunLauncher,
+              "srun_mpi"   : srunMpiLauncher,
+              "aprun"      : aprunLauncher }
--- libsubmit/launchers/launchers.py
+++ libsubmit/launchers/launchers.py
@@ -117,3 +117,36 @@ fi
 echo "Done"
 '''.format(cmd_string, taskBlocks)
     return x
+
+
+def aprunLauncher (cmd_string, taskBlocks, walltime=None):
+    ''' Worker launcher that wraps the user's cmd_string with the Aprun launch framework
+    to launch multiple cmd invocations in parallel on a single job allocation.
+
+    Args:
+        - cmd_string (string): The command string to be launched
+        - taskBlock (string) : bash evaluated string.
+
+    KWargs:
+        - walltime (int) : This is not used by this launcher.
+    '''
+
+    x = '''export CORES=$SLURM_CPUS_ON_NODE
+export NODES=$SLURM_JOB_NUM_NODES
+
+echo "Found cores : $CORES"
+echo "Found nodes : $NODES"
+WORKERCOUNT={1}
+
+cat << APRUN_EOF > cmd_$APRUN_JOB_NAME.sh
+{0}
+APRUN_EOF
+chmod a+x cmd_$APRUN_JOB_NAME.sh
+
+TASKBLOCKS={1}
+
+aprun -n $TASKBLOCKS -l bash cmd_$APRUN_JOB_NAME.sh
+
+echo "Done"
+'''.format(cmd_string, taskBlocks)
+    return x
